0dd4dc656d91c6db284044645c1d7843694b1d6e
Purushoth88 <purush_it2007@yahoo.co.in>
2016-09-07 15:31:01 +0530

    Update SampleSauceTest.java

--- SampleSauceTest.java
+++ SampleSauceTest.java
@@ -8,7 +8,7 @@ public class SampleSauceTest {
 
   public static final String USERNAME = "YOUR_USERNAME";
   public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
-  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+  public static final String URL = "http://Purush_12:3b1c6846-0b29-40e3-a87a-70d93820d78a@ondemand.saucelabs.com:80/wd/hub";
 
   public static void main(String[] args) throws Exception {
 
